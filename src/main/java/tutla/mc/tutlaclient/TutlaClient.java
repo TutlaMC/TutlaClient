e356a3c4279c56f897fed450c1db7ac3dfc3bd99
I-No-oNe <145749961+I-No-oNe@users.noreply.github.com>
2024-05-09 20:28:53 +0300

    fr

--- src/main/java/tutla/mc/tutlaclient/TutlaClient.java
+++ src/main/java/tutla/mc/tutlaclient/TutlaClient.java
@@ -1,9 +1,5 @@
 package tutla.mc.tutlaclient;
-import io.github.itzispyder.improperui.script.CallbackHandler;
-import io.github.itzispyder.improperui.script.CallbackListener;
-import io.github.itzispyder.improperui.script.events.MouseEvent;
-import io.github.itzispyder.improperui.util.ChatUtils;
-import net.fabricmc.api.ModInitializer;
+
 import io.github.itzispyder.improperui.ImproperUIAPI;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -11,7 +7,6 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
-import tutla.mc.tutlaclient.UICallbacks;
 
 public class TutlaClient implements ModInitializer {
     public static final KeyBinding BIND = KeyBindingHelper.registerKeyBinding(new KeyBinding(
@@ -20,15 +15,14 @@ public class TutlaClient implements ModInitializer {
             GLFW.GLFW_KEY_EQUAL,
             "binds.tutlaclient"
     ));
+
     @Override
     public void onInitialize() {
         System.out.println("it works but doesnt");
-        ImproperUIAPI.init("tutla-client", TutlaClient.class,
-                "scripts/main.ui"
-        );
+        ImproperUIAPI.init("tutla-client", TutlaClient.class, "scripts/main.ui");
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (BIND.wasPressed()) {
-                ImproperUIAPI.parseAndRunFile("tutla-client", "main.ui");
+                ImproperUIAPI.parseAndRunFile("tutla-client", "main.ui", new UICallbacks());
             }
         });
     }
