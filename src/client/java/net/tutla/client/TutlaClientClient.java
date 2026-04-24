6c7c0f311acfc835287e051174c5d3c7a16123ff
TutlaMC <ayaanibrahim1110@gmail.com>
2026-04-25 01:34:33 +0530

    revamp

---
+++ src/client/java/net/tutla/client/TutlaClientClient.java
@@ -0,0 +1,10 @@
+package net.tutla.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutlaClientClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
